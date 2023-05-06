b103a889e18e5ee672969d627568970f1538549d
Quique <jguirod752a@ieslacampina.es>
2023-05-06 15:35:32 +0200

    session start

--- index.php
+++ index.php
@@ -4,6 +4,9 @@
 
 	$sqljoin= "Select * from autores,libros, editorial where libros.id_autor=autores.id_autor and libros.id_editorial=editorial.id_editorial";
 	$todo= $mysqli->query($sqljoin);
+	session_start();
+	$idusu=$_SESSION["id"];
+	$datosse="Select * from usuarios where id like $idusu"
 	?>
 
 
@@ -33,7 +36,6 @@
 				} );
 		</script>
 		<style>
-
 		</style>
 		
 	</head>
@@ -58,7 +60,7 @@
 						<th>Editorial</th>
 						<th>ISBN</th>
 						<th>Cant. disponible</th>
-						<th></th>
+						<th>Comprar</th>
 						
 					</tr>
 					
@@ -75,9 +77,11 @@
 							echo"<td>",$fila['ISBN'],"</td>";
 							echo"<td>",$fila['Cantidad_dis'],"</td>";
 						
-							
-							echo"<td><a href='comprar.php?id=$fila[id_libro]' class='btn btn-danger'>Comprar</a></td>";
-						
+							if (isset($_SESSION)){
+							echo"<td><a href='comprar.php?id=$fila[id_libro]&idusu=$idusu' class='btn btn-danger'>Comprar</a></td>";
+							}else{
+								echo "<td>Logueate para comprar</td>";
+							}
 
 						echo "</tr>";
 					}
