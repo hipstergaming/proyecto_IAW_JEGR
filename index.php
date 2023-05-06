b07a75326110ea7e1c683bdf3fac5e847d05d6d5
Quique <jguirod752a@ieslacampina.es>
2023-05-06 16:34:48 +0200

    problemas con sesion

--- index.php
+++ index.php
@@ -1,111 +1,113 @@
 <?php
-    //Establezco conexion
-    require 'conexion.php';
+//Establezco conexion
+require 'conexion.php';
 
-	$sqljoin= "Select * from autores,libros, editorial where libros.id_autor=autores.id_autor and libros.id_editorial=editorial.id_editorial";
-	$todo= $mysqli->query($sqljoin);
-	
-	if(isset($_SESSION)){
-	session_start();
-	$idusu=$_SESSION["id"];
-	$datosse="Select * from usuarios where id like $idusu";
-	}
-	?>
+$sqljoin = "Select * from autores,libros, editorial where libros.id_autor=autores.id_autor and libros.id_editorial=editorial.id_editorial";
+$todo = $mysqli->query($sqljoin);
+session_start();
+
+if (isset($_SESSION['usuario'])) {
+	$idusu = $_SESSION["id"];
+	$datosse = "Select * from usuarios where id like $idusu";
+}
+?>
 
 
 <!doctype html>
 <html lang="es">
-	<head>
-		<!-- Required meta tags -->
-		<meta charset="utf-8">
-		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-		
-		<!-- Bootstrap CSS -->
-		<link rel="stylesheet" href="css/bootstrap.min.css">
-		<link rel="stylesheet" href="css/jquery.dataTables.min.css">
 
-		<!-- Optional JavaScript -->
-		<!-- jQuery first, then Popper.js, then Bootstrap JS -->
-		<script src="js/jquery-3.4.1.min.js" ></script>
-		<script src="js/bootstrap.min.js" ></script>
-		<script src="js/jquery.dataTables.min.js" ></script>
-		
-		<title>Librería Cosmere</title>
+<head>
+	<!-- Required meta tags -->
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-		<script>
-				$(document).ready( function () {
-				$('#tabla').DataTable();
-				} );
-		</script>
+	<!-- Bootstrap CSS -->
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<link rel="stylesheet" href="css/jquery.dataTables.min.css">
 
-		<style>
-		</style>
-		
-	</head>
-	<body>
-	
+	<!-- Optional JavaScript -->
+	<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+	<script src="js/jquery-3.4.1.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+	<script src="js/jquery.dataTables.min.js"></script>
+
+	<title>Librería Cosmere</title>
+
+	<script>
+		$(document).ready(function() {
+			$('#tabla').DataTable();
+		});
+	</script>
+
+	<style>
+	</style>
+
+</head>
+
+<body>
+
 	<?php
-	if(isset($_SESSION["usuario"])){
+	if (isset($_SESSION['usuario'])) {
 	?>
 		<a href="cerradodesesion.php" class='btn btn-primary '>Cerrar sesion</a>
 	<?php
-	}else{
+	} else {
 	?>
 		<a href="login.php" class='btn btn-primary '>Iniciar sesión</a>
 		<a href="registrar.php" class='btn btn-primary '>Registrar</a>
 	<?php
 	}
 	?>
-		<div class="container">
-			<h1>Bienvenido a la libreria cosmere</h1>
-			<br>
+	<div class="container">
+		<h1>Bienvenido a la libreria cosmere</h1>
+		<br>
 
-				<div class="row">
-					<h2>Lista de libros a la venta:</h2>
-				</div>
-			<br>
-			<br>
-			
-			<table id="tabla" class="display" style="width:100%">
-				<thead>
-					<tr>
-						<th>Autor</th>
-						<th>Titulo de libro</th>
-						<th>Editorial</th>
-						<th>ISBN</th>
-						<th>Cant. disponible</th>
-						<th>Comprar</th>
-						
-					</tr>
-					
-				
-				</thead>
-				<tbody>
-					<?php
-						echo "<tr>";
-					
-					while($fila = $todo->fetch_assoc()){
-							echo"<td>",$fila['Nombre'],"</td>";
-							echo"<td>",$fila['Titulo'],"</td>";
-							echo"<td>",$fila['Nombre_ed'],"</td>";
-							echo"<td>",$fila['ISBN'],"</td>";
-							echo"<td>",$fila['Cantidad_dis'],"</td>";
-						
-							if (isset($_SESSION)){
-							echo"<td><a href='comprar.php?id=$fila[id_libro]&idusu=$idusu' class='btn btn-danger'>Comprar</a></td>";
-							}else{
-								echo "<td>Logueate para comprar</td>";
-							}
-
-						echo "</tr>";
-					}
-					?>
-				</tbody>
-			</table>
-			
+		<div class="row">
+			<h2>Lista de libros a la venta:</h2>
 		</div>
-	
-	
-    </body>
+		<br>
+		<br>
 
-    </html>
+		<table id="tabla" class="display" style="width:100%">
+			<thead>
+				<tr>
+					<th>Autor</th>
+					<th>Titulo de libro</th>
+					<th>Editorial</th>
+					<th>ISBN</th>
+					<th>Cant. disponible</th>
+					<th>Comprar</th>
+
+				</tr>
+
+
+			</thead>
+			<tbody>
+				<?php
+				echo "<tr>";
+
+				while ($fila = $todo->fetch_assoc()) {
+					echo "<td>", $fila['Nombre'], "</td>";
+					echo "<td>", $fila['Titulo'], "</td>";
+					echo "<td>", $fila['Nombre_ed'], "</td>";
+					echo "<td>", $fila['ISBN'], "</td>";
+					echo "<td>", $fila['Cantidad_dis'], "</td>";
+
+					if (isset($_SESSION['usuario'])) { 
+						echo "<td><a href='comprar.php?id=$fila[id_libro]&idusu=$idusu' class='btn btn-danger'>Comprar</a></td>";
+					} else {
+						echo "<td>Logueate para comprar</td>";
+					}
+
+					echo "</tr>";
+				}
+				?>
+			</tbody>
+		</table>
+
+	</div>
+
+
+</body>
+
+</html>
