30574273070926946fb269988c117cdc8888eab1
Quique <jguirod752a@ieslacampina.es>
2023-05-23 17:15:51 +0200

    V2, cambio de titulos

--- panel_de_control/listado_libros/editar_libro.php
+++ panel_de_control/listado_libros/editar_libro.php
@@ -20,7 +20,7 @@ $resultado2 = $mysqli->query($editorial);
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Libreria cosmere: Editar libro</title>
     <link rel="stylesheet" href="../paneldecontrol.css">
 </head>
 
