d66e658223345563572085581d121f2343dc7264
Quique <jguirod752a@ieslacampina.es>
2023-06-15 19:30:54 +0200

    No capturaba si un usuario no existia

--- complogin.php
+++ complogin.php
@@ -1,5 +1,5 @@
 <?php
-//Establezco conexion
+
 require 'conexion.php';
 $usuario = $_POST['usuario'];
 $contraseña = $_POST['contra'];
@@ -20,29 +20,40 @@ $resultado = $mysqli->query($sql);
     <link rel="stylesheet" href="estiloinicio.css">
     <title>Libreria cosmere: Login</title>
     <link rel="icon" href="images/Acero.ico" type="image/png">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+
 </head>
 
 <body>
+
     <div class="container">
         <?php
-        while (($fila = $resultado->fetch_assoc())) {
-            $hash = $fila['contraseña'];
+        if ($resultado->num_rows > 0) {
+            while (($fila = $resultado->fetch_assoc())) {
+                $hash = $fila['contraseña'];
 
-            if (password_verify($contraseña, $hash)) {
-                $encontrado = true;
-                session_start();
-                $_SESSION["id_usu"] = $fila['id_usuario'];
-                $_SESSION["usuario"] = $usuario;
-                $_SESSION["rango"] = $fila['rango'];
+                if (password_verify($contraseña, $hash)) {
+                    $encontrado = true;
+                    session_start();
+                    $_SESSION["id_usu"] = $fila['id_usuario'];
+                    $_SESSION["usuario"] = $usuario;
+                    $_SESSION["rango"] = $fila['rango'];
 
 
-                header("Location:index.php");
-            } else {
-                ?>
-                <p class="alert alert-danger">Error, usuario o contraseña incorrecta</p>
-                echo '<a href="login.php">Volver al login</a>';
-                <?php
+                    header("Location:index.php");
+                } else {
+        ?>
+                    <p class="alert alert-danger">Error contraseña incorrecta</p>
+                    <a href="login.php">Volver al login</a>;
+            <?php
+                }
             }
+        } else {
+            ?>
+            <p class="alert alert-danger">Error, ese usuario no existe</p>
+            <a href="login.php">Volver al login</a>
+        <?php
         }
 
 
